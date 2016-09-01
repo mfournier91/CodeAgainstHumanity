acf7ddcbe8fe30554e939084a4bd6f01f641f3b3
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-01 16:51:23 -0400

    Angular quiz question.

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ function CAHctrlFunction() {
   var vm = this;
   console.log('controller hit');
   vm.doEet = "Hello World";
+  var quizItems = [{question: "question1", answer: "answer1"}, {question: "question2", answer: "answer2"}, {question: "question3", answer: "answer3"}]
+  var quizItem = quizItems[Math.floor(Math.random()*quizItems.length)]
+  vm.quizQuestion = quizItem["question"];
+  vm.quizAnswer = quizItem["answer"];
+  console.log(quizItem["question"]);
+  console.log(quizItem["answer"]);
 }
